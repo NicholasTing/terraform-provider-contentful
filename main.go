6964daac26d6fcf20895b48cbbb6224070213fca
Nicholas Ting <nicholas.ting@convertdigital.com.au>
2023-09-17 19:16:02 +1000

    fix: folder rror

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/nicholasting/terraform-provider-contentful-alpha"
+	"github.com/nicholasting/terraform-provider-contentful/contentful"
 )
-
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
 
 // If you do not have terraform installed, you can remove the formatting command, but its suggested to
